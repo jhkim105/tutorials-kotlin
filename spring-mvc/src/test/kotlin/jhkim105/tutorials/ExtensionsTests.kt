2222bb0f2140bf2e630af777baea4ad57df36ed2
jhkim105 <jhkim105@gmail.com>
2023-11-06 16:10:36 +0900

    .

---
+++ spring-mvc/src/test/kotlin/jhkim105/tutorials/ExtensionsTests.kt
@@ -0,0 +1,14 @@
+package jhkim105.tutorials
+
+import org.junit.jupiter.api.Test
+
+class ExtensionsTests {
+
+
+    @Test
+    fun test() {
+        println("1111")
+        println("https://seo.tbwakorea.com/blog/what-is-slug/".toSlug())
+    }
+
+}
