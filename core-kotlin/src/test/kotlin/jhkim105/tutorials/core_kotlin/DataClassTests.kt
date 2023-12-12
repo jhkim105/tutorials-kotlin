304556004b110d460c40f9ff40f5559ee5ca3e79
jhkim105 <jhkim105@gmail.com>
2023-12-12 16:35:02 +0900

    kotlin basic

---
+++ core-kotlin/src/test/kotlin/jhkim105/tutorials/core_kotlin/DataClassTests.kt
@@ -0,0 +1,19 @@
+package jhkim105.tutorials.core_kotlin
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class DataClassTests {
+
+
+    @Test
+    fun test() {
+        val sample1 = Sample("name 01")
+        val sample2 = Sample("name 01")
+        assertTrue(sample1 == sample2)
+    }
+}
+
+data class Sample(
+    private val name: String
+)
