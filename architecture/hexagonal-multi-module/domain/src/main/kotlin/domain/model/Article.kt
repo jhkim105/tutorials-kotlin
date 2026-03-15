afcfb549b6c22ebf2f606f01450d1afeed57c37c
jhkim105 <jhkim105@gmail.com>
2026-03-15 12:41:48 +0900

    Add .gitignore for Kotlin/Gradle multi-module project

--- architecture/hexagonal-multi-module/domain/src/main/kotlin/domain/model/Article.kt
+++
@@ -1,8 +0,0 @@
-package domain.model
-
-data class Article(
-    val id: String? = null,
-    val title: String,
-    val content: String,
-    val tags: List<Tag>
-)
--- architecture/hexagonal-multi-module/domain/src/main/kotlin/domain/port/ArticlePort.kt
+++
@@ -1,8 +0,0 @@
-package domain.port
-
-import domain.model.Article
-
-interface ArticlePort {
-    fun save(article: Article): Article
-    fun findById(id: String): Article?
-}
